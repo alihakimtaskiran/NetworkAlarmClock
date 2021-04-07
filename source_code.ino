8024f0438674c5fcb8ac1ae7e5faca800e61b451
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2021-04-07 13:07:02 +0300

    Update source_code.ino

--- source_code.ino
+++ source_code.ino
@@ -1,68 +1,85 @@
+/*
+
+*/
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
-const char *ssid     = "SSID HERE";//fill here
-const char *password = "PASSWORD HERE";//fill here
+const char *ssid     = "";//Fill here network name
+const char *password = "";//Fil here password
 
+const int speaker_pin=5;//Replace here with speaker pin
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
-byte hour,minute,sec;
 
 void setup() {
+  // Initialize Serial Monitor
   Serial.begin(115200);
-  
   // Connect to Wi-Fi
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    
     delay(500);
-    
     Serial.print(".");
+   
   }
-
+  delay(5);
   timeClient.begin();
-  timeClient.setTimeOffset(10800);
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(10800);//I'm located in GMT+3
 }
 
 void loop() {
+  digitalWrite(2,LOW);
   timeClient.update();
-  hour = timeClient.getHours();
-  minute = timeClient.getMinutes();
-  sec=timeClient.getSeconds();
 
- while(hour>=8){
-  if(hour==8&&minute==0&&sec<6){
-tone(5,180);
-delay(10000);
-noTone(5);
+  int hour = timeClient.getHours();
+  Serial.print("Hour: ");
+  Serial.println(hour);  
+
+  int minute = timeClient.getMinutes();
+  Serial.print("Minutes: ");
+  Serial.println(minute); 
+   
+  int sec = timeClient.getSeconds();
+  Serial.print("Seconds: ");
+  Serial.println(sec);  
+  if(hour>7){
+    delay(5);//Extra time delays in order to avoid errors. NodeMCU has a bug which reboots the system as we perform new logic expression in another logic expression
+    if(hour==8&&minute==0&&sec<7){
+      //waking up
+      tone(speaker_pin,180);
+      delay(10000);
+      noTone(speaker_pin);
+    }
+    else if(minute==0&&sec<7){//3 short beep hourly
+      tone(speaker_pin,271);
+      delay(500);
+      noTone(speaker_pin);
+      delay(500);
+      tone(speaker_pin,271);
+      delay(500);
+      noTone(speaker_pin);
+      delay(500);
+      tone(speaker_pin,271);
+      delay(500);
+      noTone(speaker_pin);
+      delay(1500);
+    }
+    else if(minute%15==0&&sec<7){//A beep for every 15 minutes
+      tone(speaker_pin,271);
+      delay(1000);
+      noTone(speaker_pin);  
+      delay(6000);    
+    }
+    else{
+      delay(50);
+      }
   }
-  if(minute==0&&sec<7){
-    tone(5,271);
-    delay(500);
-    noTone(5);
-    tone(5,271);
-    delay(500);
-    noTone(5);
-    tone(5,271);
-    delay(500);
-    noTone(5);
-  }
-  
-  else if(minute%15==0&&sec<7){
-    tone(5,271);
-    delay(1000);
-    noTone(5);
-    delay(6000);
-  }
-  else
-  {}
-    
+  delay(5000);
 }
-   delay(5000);
- }
-
-
-
