203382c998887404329457c5c92580184cba2cb5
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2021-04-13 16:04:38 +0300

    Update source_code.ino

--- source_code.ino
+++ source_code.ino
@@ -1,13 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
 
 const char *ssid     = "";//Fill here network name
 const char *password = "";//Fil here password
-
+const char *domain="[]";//http://[].local mDNS adress for NodeMCU server. You
 const int speaker_pin=5;//Replace here with speaker pin
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+WiFiServer server(80);
 
 void setup() {
   // Initialize Serial Monitor
@@ -15,13 +19,17 @@ void setup() {
   // Connect to Wi-Fi
   Serial.print("Connecting to ");
   Serial.println(ssid);
+    WiFi.mode(WIFI_STA);
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-   
   }
+  
   delay(5);
+     Serial.println("Connected");
+
   timeClient.begin();
   // Set offset time in seconds to adjust for your timezone, for example:
   // GMT +1 = 3600
@@ -29,9 +37,34 @@ void setup() {
   // GMT -1 = -3600
   // GMT 0 = 0
   timeClient.setTimeOffset(10800);//I'm located in GMT+3
+
+    if (!MDNS.begin(domain)) {
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+
+  // Start TCP (HTTP) server
+  server.begin();
+  Serial.println("TCP server started");
+
+  // Add service to MDNS-SD
+  MDNS.addService("http", "tcp", 80);
 }
 
 void loop() {
+    MDNS.update();
+
+if(millis()%5000<500){
+  handle_timer();
+}
+else{
+Serial.println("Iterating");}
+}
+
+void handle_timer(){
   timeClient.update();
 
   int hour = timeClient.getHours();
@@ -65,17 +98,14 @@ void loop() {
       tone(speaker_pin,271);
       delay(500);
       noTone(speaker_pin);
-      delay(1500);
     }
     else if(minute%15==0&&sec<7){//A beep for every 15 minutes
       tone(speaker_pin,271);
       delay(1000);
       noTone(speaker_pin);  
-      delay(6000);    
     }
     else{
-      delay(50);
+      delay(1);
       }
   }
-  delay(5000);
 }
